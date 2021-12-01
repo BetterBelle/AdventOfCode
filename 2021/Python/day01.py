6e8affb7a04523b3644910465e49f2c955014eda
Cloudan29 <guitarman.dan29@gmail.com>
2021-12-01 12:43:27 -0500

    day01 of 2021 solution

---
+++ 2021/Python/day01.py
@@ -0,0 +1,28 @@
+inp = open("2021/inputs/day01.txt")
+measurements = list()
+
+for line in inp:
+    measurements.append(int(line))
+
+def part1():
+    ans = 0
+    for i in range(1, len(measurements)):
+        if measurements[i - 1] < measurements[i]:
+            ans += 1
+
+    return ans
+
+
+def part2():
+    ans = 0
+    for i in range(1, len(measurements) - 2):
+        prev = sum(measurements[i-1:i+2])
+        curr = sum(measurements[i:i+3])
+        if prev < curr:
+            ans += 1
+            
+    return ans
+
+
+print (part1())
+print (part2())
