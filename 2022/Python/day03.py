0e6448d43b52244fac7be66eeaec6d8b30956c0f
Dan <guitarman.dan29@gmail.com>
2022-12-03 00:56:15 -0500

    day 3 Python now uses set intersections

--- 2022/Python/day03.py
+++ 2022/Python/day03.py
@@ -1,43 +1,23 @@
 inp = [line.strip() for line in open("2022/inputs/day03.txt")]
 
-prios = list()
-for line in inp:
-    next_prios = list()
-    for char in line:
-        if ord(char) >= 97:
-            next_prios.append(ord(char) - 96)
-        else:
-            next_prios.append(ord(char) - 38)
-
-    prios.append(next_prios)
+def get_val(char):
+    return ord(char) - ord('a') + 1 if char.islower() else ord(char) - ord('A') + 27
 
 def part1():
     sum_prios = 0
-    next_found = 0
-    for prio in prios:
-        sack1, sack2 = prio[:int(len(prio)/2)], prio[int(len(prio)/2):]
-        for item in sack1:
-            if item in sack2:
-                next_found = item
-
-        sum_prios += next_found
+    for bag in inp:
+        bag1, bag2 = bag[:len(bag)//2], bag[len(bag)//2:]
+        found, = set(bag1) & set(bag2)
+        sum_prios += get_val(found)
 
     return sum_prios
 
 def part2():
     sum_prios = 0
-    next_found = 0
-    next_group = list()
-    for prio in prios:
-        next_group.append(prio)
-
-        if len(next_group) == 3:
-            for item in next_group[0]:
-                if item in next_group[1] and item in next_group[2]:
-                    next_found = item
-                    
-            sum_prios += next_found
-            next_group = list()
+    for i in range(0, len(inp), 3):
+        elf1, elf2, elf3 = inp[i:i+3]
+        found, = set(elf1) & set(elf2) & set(elf3)
+        sum_prios += get_val(found)
 
     return sum_prios
 
